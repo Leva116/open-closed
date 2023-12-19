18a42266685fe96f943c63dced5bdc01c4e1ea58
leva6 <leva6@DESKTOP-4B7ROF8>
2023-12-19 15:19:11 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace open_closed
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    public class EmployeeReport
+    {
+        // <summary>
+        /// Тип отчета
+        /// </summary>
+        public string TypeReport { get; set; }
+
+        /// <summary>
+        /// Отчет по сотруднику
+        /// </summary>
+        public void GenerateReport(Employee em)
+        {
+            if (TypeReport == "CSV")
+            {
+                // Генерация отчета в формате CSV
+            }
+
+            if (TypeReport == "PDF")
+            {
+                // Генерация отчета в формате PDF
+            }
+        }
+        public class IEmployeeReport
+        {
+            /// <summary>
+            /// Метод для создания отчета
+            /// </summary>
+            public virtual void GenerateReport(Employee em)
+            {
+                // Базовая реализация, которую нельзя модифицировать
+            }
+        }
+
+        public class EmployeeCSVReport : IEmployeeReport
+        {
+            public override void GenerateReport(Employee em)
+            {
+                // Генерация отчета в формате CSV
+            }
+        }
+
+        public class EmployeePDFReport : IEmployeeReport
+        {
+            public override void GenerateReport(Employee em)
+            {
+                // Генерация отчета в формате PDF
+            }
+        }
+    }
+
+    public class Employee
+    {
+    }
+
+
+}
